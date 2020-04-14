54deb08569e3e0ae94f49be69bc32fd057e51aa4
dawidbrzozowski <dawidbrzozowski98@gmail.com>
2020-04-14 18:17:45 +0200

    added token types

---
+++ lexer/token_type.py
@@ -0,0 +1,60 @@
+from enum import Enum, auto
+
+
+class TokenType(Enum):
+    # data types
+    T_INT = auto()
+    T_DOUBLE = auto()
+    T_CHAR = auto()
+    T_BOOL = auto()
+
+    # const values
+    T_CHARACTER = auto()
+    T_FLOAT_NUM = auto()
+    T_INT_NUM = auto()
+
+    # punctuation
+    T_COMMA = auto()
+    T_DOT = auto()
+    T_COLON = auto()
+    T_SEMICOLON = auto()
+    T_LBRACKET = auto()
+    T_RBRACKET = auto()
+    T_LPARENT = auto()
+    T_RPARENT = auto()
+
+    # math operations
+    T_PLUS = auto()
+    T_MINUS = auto()
+    T_MUL = auto()
+    T_DIV = auto()
+
+    # math order types
+    T_LESS_OR_EQ = auto()
+    T_LESS = auto()
+    T_GREATER_OR_EQ = auto()
+    T_GREATER = auto()
+    T_EQ = auto()
+    T_NOT_EQ = auto()
+
+    # logical types
+    T_OR = auto()
+    T_AND = auto()
+    T_NOT = auto()
+
+    # other
+    T_ID = auto()
+    T_EOT = auto()
+    T_IF = auto()
+    T_ELSE = auto()
+    T_TRUE = auto()
+    T_FALSE = auto()
+    T_RETURN = auto()
+    T_WHILE = auto()
+    T_ASSIGN = auto()
+    T_VOID = auto()
+    T_FUNCTION = auto()
+
+    # tokens specific to task
+    T_UNIT = auto()
+    T_PHYS = auto()
