a9bf50c744c033764ad6a1d61d589975d880d301
dawidbrzozowski <dawidbrzozowski98@gmail.com>
2020-04-16 16:19:50 +0200

    added regex to token and tests

---
+++ tests/regex_tests.py
@@ -0,0 +1,272 @@
+import re
+import unittest
+from lexer.regex2token import regex2token
+from lexer.token import Token
+from lexer.token_type import TokenType
+
+
+class TestRegex(unittest.TestCase):
+    def setUp(self) -> None:
+        self.regex2token = regex2token
+        self.regex2token_compiled = {}
+        for regex in regex2token:
+            regex_compiled = re.compile(regex)
+            self.regex2token_compiled[regex_compiled] = regex2token[regex]
+
+    def _find_matching_token_args(self, line):
+        for regex in self.regex2token_compiled:
+            match = regex.match(line)
+            if match:
+                token_type = self.regex2token_compiled[regex]
+                value = match.group(0)
+                return token_type, value
+        return None
+
+    def find_token(self, line):
+        token_args = self._find_matching_token_args(line)
+        if not token_args:
+            return None
+        token_type, value = token_args
+        if token_type.name.startswith("V"):
+            token = Token(token_type, value)
+        else:
+            token = Token(token_type)
+        return token
+
+    def test_double_value(self):
+        line = "2.5"
+        token = self.find_token(line)
+        expected = Token(TokenType.VT_DOUBLE, "2.5")
+        self.assertEqual(expected, token)
+
+    def test_string_value(self):
+        line = '"test string"'
+        token = self.find_token(line)
+        expected = Token(TokenType.VT_STRING, '"test string"')
+        self.assertEqual(expected, token)
+
+    def test_char_value(self):
+        line = "'c'"
+        token = self.find_token(line)
+        expected = Token(TokenType.VT_CHAR, "'c'")
+        self.assertEqual(expected, token)
+
+    def test_int_value(self):
+        line = "152"
+        token = self.find_token(line)
+        expected = Token(TokenType.VT_INT, "152")
+        self.assertEqual(expected, token)
+
+    def test_int(self):
+        line = "int"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_INT)
+        self.assertEqual(expected, token)
+
+    def test_double(self):
+        line = "double"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_DOUBLE)
+        self.assertEqual(expected, token)
+
+    def test_char(self):
+        line = "char"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_CHAR)
+        self.assertEqual(expected, token)
+
+    def test_string(self):
+        line = "string"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_STRING)
+        self.assertEqual(expected, token)
+
+    def test_bool(self):
+        line = "bool"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_BOOL)
+        self.assertEqual(expected, token)
+
+    def test_comma(self):
+        line = ","
+        token = self.find_token(line)
+        expected = Token(TokenType.T_COMMA)
+        self.assertEqual(expected, token)
+
+    def test_dot(self):
+        line = "."
+        token = self.find_token(line)
+        expected = Token(TokenType.T_DOT)
+        self.assertEqual(expected, token)
+
+    def test_colon(self):
+        line = ":"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_COLON)
+        self.assertEqual(expected, token)
+
+    def test_semicolon(self):
+        line = ";"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_SEMICOLON)
+        self.assertEqual(expected, token)
+
+    def test_lbracket(self):
+        line = "{"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_LBRACKET)
+        self.assertEqual(expected, token)
+
+    def test_rbracket(self):
+        line = "}"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_RBRACKET)
+        self.assertEqual(expected, token)
+
+    def test_lparent(self):
+        line = "("
+        token = self.find_token(line)
+        expected = Token(TokenType.T_LPARENT)
+        self.assertEqual(expected, token)
+
+    def test_rparent(self):
+        line = ")"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_RPARENT)
+        self.assertEqual(expected, token)
+
+    def test_plus(self):
+        line = "+"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_PLUS)
+        self.assertEqual(expected, token)
+
+    def test_minus(self):
+        line = "-"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_MINUS)
+        self.assertEqual(expected, token)
+
+    def test_mul(self):
+        line = "*"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_MUL)
+        self.assertEqual(expected, token)
+
+    def test_div(self):
+        line = "/"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_DIV)
+        self.assertEqual(expected, token)
+
+    def test_less_or_eq(self):
+        line = "<="
+        token = self.find_token(line)
+        expected = Token(TokenType.T_LESS_OR_EQ)
+        self.assertEqual(expected, token)
+
+    def test_less(self):
+        line = "<"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_LESS)
+        self.assertEqual(expected, token)
+
+    def test_eq(self):
+        line = "=="
+        token = self.find_token(line)
+        expected = Token(TokenType.T_EQ)
+        self.assertEqual(expected, token)
+
+    def test_not_eq(self):
+        line = "!="
+        token = self.find_token(line)
+        expected = Token(TokenType.T_NOT_EQ)
+        self.assertEqual(expected, token)
+
+    def test_greater_or_eq(self):
+        line = ">="
+        token = self.find_token(line)
+        expected = Token(TokenType.T_GREATER_OR_EQ)
+        self.assertEqual(expected, token)
+
+    def test_greater(self):
+        line = ">"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_GREATER)
+        self.assertEqual(expected, token)
+
+    def test_or(self):
+        line = "or"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_OR)
+        self.assertEqual(expected, token)
+
+    def test_and(self):
+        line = "and"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_AND)
+        self.assertEqual(expected, token)
+
+    def test_not(self):
+        line = "not"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_NOT)
+        self.assertEqual(expected, token)
+
+    def test_if(self):
+        line = "if"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_IF)
+        self.assertEqual(expected, token)
+
+    def test_else(self):
+        line = "else"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_ELSE)
+        self.assertEqual(expected, token)
+
+    def test_true(self):
+        line = "true"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_TRUE)
+        self.assertEqual(expected, token)
+
+    def test_false(self):
+        line = "false"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_FALSE)
+        self.assertEqual(expected, token)
+
+    def test_return(self):
+        line = "return"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_RETURN)
+        self.assertEqual(expected, token)
+
+    def test_while(self):
+        line = "while"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_WHILE)
+        self.assertEqual(expected, token)
+
+    def test_assign(self):
+        line = "="
+        token = self.find_token(line)
+        expected = Token(TokenType.T_ASSIGN)
+        self.assertEqual(expected, token)
+
+    def test_void(self):
+        line = "void"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_VOID)
+        self.assertEqual(expected, token)
+
+    def test_function(self):
+        line = "function"
+        token = self.find_token(line)
+        expected = Token(TokenType.T_FUNCTION)
+        self.assertEqual(expected, token)
+
+
+if __name__ == '__main__':
+    unittest.main()
